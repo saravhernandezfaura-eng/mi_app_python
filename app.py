65ac1dba3e15ecd68e52821ac09e6eeb128fb98e
sara <saraventinahernandezfaura@gmail.com>
2026-05-08 23:12:38 -0500

    agrego menu principal

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import streamlit as st
 
-st.title('Mi primera aplicación')
-nombre = st.text_input('Escribe tu nombre')
+st.title("Mis Aplicaciones Python")
 
-if nombre:
-    st.write(f'Hola {nombre}')
+pagina = st.selectbox("Selecciona una aplicación", [
+    "Inicio",
+    "Clasificador de números",
+    "Calculadora de IMC",
+    "Factorial",
+    "Tabla de multiplicar"
+])
+
+if pagina == "Clasificador de números":
+    exec(open("numeros.py").read())
+elif pagina == "Calculadora de IMC":
+    exec(open("imc.py").read())
+elif pagina == "Factorial":
+    exec(open("factorial.py").read())
+elif pagina == "Tabla de multiplicar":
+    exec(open("tabla_multiplicar.py").read())
+else:
+    st.write("👈 Selecciona una aplicación del menú para comenzar")
+    
 
