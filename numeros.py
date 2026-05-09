4c5ae5e852f863b0dee249106fa0ac617a6dff36
sara <saraventinahernandezfaura@gmail.com>
2026-05-08 23:23:03 -0500

    arreglo clasificador numeros

--- numeros.py
+++ numeros.py
@@ -1,8 +1,13 @@
-numero = float(input("Ingresa un número: "))
+import streamlit as st
 
-if numero > 0:
-    print("El número es positivo")
-elif numero < 0:
-    print("El número es negativo")
-else:
-    print("El número es cero")
+st.title("Clasificador de números")
+
+numero = st.number_input("Ingresa un número", step=0.1)
+
+if st.button("Clasificar"):
+    if numero > 0:
+        st.success("El número es positivo")
+    elif numero < 0:
+        st.error("El número es negativo")
+    else:
+        st.warning("El número es cero")
